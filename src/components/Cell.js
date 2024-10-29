d363ad14e1bf7f3c357afb0b40c5210c2dd09faf
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-10-30 00:58:43 +0530

    Added Star & Arrow icons in Cells component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,12 +1,34 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Colors } from '../constants/Colors';
 import Pile from './Pile';
+import { ArrowSpot, SafeSpots, StarSpots } from '../helpers/PlotData';
+import { ArrowRightIcon, StarIcon } from 'react-native-heroicons/outline'
+import { RFValue } from 'react-native-responsive-fontsize'
 
 const Cell = ({ color, id }) => {
+
+    const isSafeSpot = useMemo(() => SafeSpots.includes(id), [id]);
+    const isStarSpot = useMemo(() => StarSpots.includes(id), [id]);
+    const isArrowSpot = useMemo(() => ArrowSpot.includes(id), [id]);
+
+
     return (
-        <View style={styles.container}>
-            <Text>{ id}</Text>
+        <View style={[styles.container, { backgroundColor: isSafeSpot ? color : 'white' }]}>
+        
+            {isStarSpot && <StarIcon size={20} color="grey" />}
+            {isArrowSpot && (<ArrowRightIcon style={{
+                color: color,
+                transform: [
+                    {
+                        rotate:id===38 ? '180deg':id==25 ? '90deg' :id===51 ? '-90deg':'0deg',
+                    },
+                ],
+            }}
+                size={RFValue(18)} />
+            )}
+
+            {/* <Text>{ id}</Text> */}
             {/* <Pile
                 cell={true}
                 player={2}
@@ -15,7 +37,7 @@ const Cell = ({ color, id }) => {
                 color={Colors.green}
             /> */}
         </View>
-    )
+    );
 };
 
 const styles = StyleSheet.create({

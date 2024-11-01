a04bd4c7b48003bff3e48d0e2221edad31c59071
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-11-02 01:36:18 +0530

    Add reducer for preserve state in reducers

--- src/redux/reducers/gameSlice.js
+++ src/redux/reducers/gameSlice.js
@@ -6,9 +6,41 @@ export const gameSlice = createSlice({
     initialState: intiialState,
     reducers: {
         resetGame: () => intiialState,
+        updateDiceNo: (state, action) => {
+            state.diceNo = action.payload.diceNo;
+            state.isDiceRolled = true;
+        },
+        enablePileSelection: (state, action) => {
+            state.touchDiceBlock = true;
+            state.pileSelectionPlayer = action.payload.playerNo;
+        },
+        enableCellSelection: (state, action) => {
+            state.touchDiceBlock = true;
+            state.cellSelectionPlayer = action.payload.playerNo;
+        },
+        disableTouch: state => {
+            state.touchDiceBlock = true;
+            state.cellSelectionPlayer = -1;
+            state.pileSelectionPlayer = -1;
+        },
+        unfreezeDice: state => {
+            state.touchDiceBlock = false;
+            state.isDiceRolled = false;
+        },
+        updateFireworks: (state, action) => {
+            state.fireworks = action.payload;
+        },
+        announceWinner: (state, action) => {
+            state.winner = action.payload;
+        },
+        updatePlayerChance: (state, action) => {
+            state.chancePlayer = action.payload.chancePlayer;
+            state.touchDiceBlock = false;
+            state.isDiceRolled = false;
+        },
     },
 });
 
-export const { resetGame } = gameSlice.actions;
+export const { resetGame, updateDiceNo, announceWinner, updateFireworks, updatePlayerChance, unfreezeDice, disableTouch, enableCellSelection, enablePileSelection } = gameSlice.actions;
 
 export default gameSlice.reducer;

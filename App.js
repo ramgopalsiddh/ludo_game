dffd3b510ce4d06722180ed9661083f66dde17c8
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-11-01 20:26:14 +0530

    Add navigation & update root route

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import LudoboardScreen from './src/screens/LudoboardScreen'
+import { Provider } from 'react-redux';
+import { persistor, store } from './src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Navigation from './src/navigation/Navigation';
 
 const App = () => {
   return (
-      <LudoboardScreen />
-  )
-}
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
+    </Provider>
+  );
+};
 
-export default App
+export default App;

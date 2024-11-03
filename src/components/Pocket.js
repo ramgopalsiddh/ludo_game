2e29e0025aa6586021646234de34897697534852
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-11-03 23:01:00 +0530

    Update logic for piles selection in Pocket

--- src/components/Pocket.js
+++ src/components/Pocket.js
@@ -2,28 +2,63 @@ import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import Pile from './Pile';
+import { useDispatch } from 'react-redux';
+import { unfreezeDice, updatePlayerChance, updatePlayerPiceValue } from '../redux/reducers/gameSlice';
+import { startingPoints } from '../helpers/PlotData';
 
-const Pocket = React.memo(({ color, player }) => {
+const Pocket = React.memo(({ color, player, data }) => {
+
+    const dispatch = useDispatch()
+    const handlePress = async value => {
+        let playerNo = value?.id[0]
+        switch (playerNo) {
+            case 'A':
+                playerNo = 'player1'
+                break;
+            case 'B':
+                playerNo = 'player2'
+                break;
+            case 'C':
+                playerNo = 'player3'
+                break;
+            case 'D':
+                playerNo = 'player4'
+                break;
+        }
+
+        dispatch(updatePlayerPiceValue({
+            playerNo: playerNo,
+            pieceId: value.id,
+            pos: startingPoints[parseInt(playerNo.match(/\d+/)[0], 10) - 1],
+            travelCount: 1,
+        }),
+        );
+
+        dispatch(unfreezeDice());
+    };
+
     return (
         <View style={[styles.container, { backgroundColor: color }]}>
             <View style={styles.childFrame}>
                 <View style={styles.flexRow}>
-                    <Plot piceNo={0} player={player} color={color} />
-                    <Plot piceNo={1} player={player} color={color} />
+                    <Plot piceNo={0} data={data} onPress={handlePress} player={player} color={color} />
+                    <Plot piceNo={1} data={data} onPress={handlePress} player={player} color={color} />
                 </View>
                 <View style={[styles.flexRow, {marginTop: 20}]}>
-                    <Plot piceNo={0} player={player} color={color} />
-                    <Plot piceNo={1} player={player} color={color} />
+                    <Plot piceNo={2} data={data} onPress={handlePress} player={player} color={color} />
+                    <Plot piceNo={3} data={data} onPress={handlePress} player={player} color={color} />
                 </View>
             </View>
         </View>
     )
 });
 
-const Plot = ({ piceNo, player, color }) => {
+const Plot = ({ piceNo, player, color, data, onPress }) => {
     return (
-        <View style={[styles.plot, {backgroundColor: color}]}>
-            <Pile color={color} player={player} />
+        <View style={[styles.plot, { backgroundColor: color }]}>
+            {data && data[piceNo]?.pos === 0 && (
+                <Pile color={color} player={player} onPress={()=>onPress(data[piceNo])} />
+            )}
         </View>
     )
 }
